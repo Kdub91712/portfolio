a7e71ac1bf8225e682c541392a03d09106058eb2
Kevin Wilson <kev1583@gmail.com>
2020-04-26 16:18:47 -0500

    adding youtube intro

--- src/components/about.js
+++ src/components/about.js
@@ -14,6 +14,10 @@ export default class About extends Component {
                         <p>I've been in software development for 7+ years.  I'm most experienced in LAMP stack technologies, APIs and Integrations.</p>
                         <p>PHP is my strongest language, but I'm really enjoying working with Python and React.  In my spare time I like to work on app ideas and trying new technologies.</p>
                         <p>I grew up in the southwest suburbs of Chicago, and have lived in the city 10+ years.  I enjoy music, investing and the great outdoors.</p>
+                        <h3>About this Website</h3>
+                        <div>
+                            <iframe title="intro" width="325" height="182.81" src="https://www.youtube.com/embed/gqOrZ2lxcX0" frameBorder="2" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </div>
                     </div>
                 </div>
             </div>
