717d8fa27ad130b025c4f4274d3a86f28ef6f635
Kevin Wilson <kevinwilson@Kevins-MacBook-Air.local>
2025-05-25 19:04:53 -0600

    copy updates

--- src/components/about.js
+++ src/components/about.js
@@ -11,9 +11,9 @@ export default class About extends Component {
                     <div className="text-area">
                         <h3>About Kevin</h3>
                         <img id="profile-pic" src={profilePicture} alt="kevin"></img>
-                        <p>I've been in software development for 7+ years.  I'm most experienced in LAMP stack technologies, APIs and Integrations.</p>
+                        <p>I've been in software development for 12+ years.  I'm most experienced in LAMP stack technologies, APIs and Integrations.</p>
                         <p>PHP is my strongest language, but I'm really enjoying working with Python and React.  In my spare time I like to work on app ideas and trying new technologies.</p>
-                        <p>I grew up in the southwest suburbs of Chicago, and have lived in the city 10+ years.  I enjoy music, investing and the great outdoors.</p>
+                        <p>I grew up in the southwest suburbs of Chicago, and lived in the city 10+ years.  I now reside in Colorado where I mountain bike frequently. I also enjoy music, investing and the great outdoors.</p>
                         <h3>About this Website</h3>
                         <div>
                             <iframe title="intro" width="325" height="182.81" src="https://www.youtube.com/embed/gqOrZ2lxcX0" frameBorder="2" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,7 +9,7 @@ export default class Navigation extends Component {
                 <a className="nav-links" href="/" onClick={(e) => this.props.navLinkHandler(e, "about")}>About</a>
                 <a className="nav-links" href="/" onClick={(e) => this.props.navLinkHandler(e, "skills")}>Skills</a>
                 <a className="nav-links" href="/" onClick = {(e) => this.props.navLinkHandler(e, "projects")}>Projects</a>
-                <a className="nav-links" href="https://www.linkedin.com/in/kevinwilsonchicago/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a className="nav-links" href="https://www.linkedin.com/in/kevinwilsoncolorado/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                 <a className="nav-links" href="https://github.com/Kdub91712" target="_blank" rel="noopener noreferrer">Github</a>
                 <a className="nav-links" href="/" onClick = {(e) => this.props.navLinkHandler(e, "contact")}>Contact</a>
             </nav>
