5ca301366f96e944f0788c34e053cb561c9430c6
Kevin Wilson <kev1583@gmail.com>
2026-04-29 11:24:38 -0500

    fixing tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+jest.mock('react-ga', () => ({
+  initialize: jest.fn(),
+  pageview: jest.fn(),
+  event: jest.fn(),
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
